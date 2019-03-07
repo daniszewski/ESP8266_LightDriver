e960632ef95e5c962a104c8c83a0c9b3516d4d11
Mariusz Daniszewski <daniszewski@gmail.com>
2019-03-07 15:40:45 +0100

    Firmware update through HTTP

--- src/LocalStorage.cpp
+++ src/LocalStorage.cpp
@@ -1,9 +1,12 @@
 #include "LocalStorage.h"
 
 #define FORMAT_INDICATOR "/mac"
+#define FIRMWARE "/www/firmware.bin"
 
 void LocalStorageClass::begin() {
     SPIFFS.begin();
+
+    // file system format
     if (!SPIFFS.exists(FORMAT_INDICATOR)) {
         Serial.println("SPIFFS: formatting");
         if (SPIFFS.format()) {
@@ -14,6 +17,26 @@ void LocalStorageClass::begin() {
             }
             else Serial.println("SPIFFS: formatting error (creating indicator file)");
         } else Serial.println("SPIFFS: formatting error");
+    }
+
+    // firmware upgrade
+    if (SPIFFS.exists(FIRMWARE)) {
+        uint32_t maxSketchSpace = (ESP.getFreeSketchSpace() - 0x1000) & 0xFFFFF000;
+        if (!Update.begin(maxSketchSpace, U_FLASH)) { //start with max available size
+            Update.printError(Serial);
+            Serial.println("ERROR");
+        } else {
+            File f = SPIFFS.open(FIRMWARE, "r");
+            while (f.available()) {
+                uint8_t ibuffer[128];
+                f.read((uint8_t *)ibuffer, 128);
+                Serial.println((char *)ibuffer);
+                Update.write(ibuffer, sizeof(ibuffer));
+            }
+            Serial.print(Update.end(true));
+            f.close();
+        }
+        SPIFFS.remove(FIRMWARE);
     }
 }
 
