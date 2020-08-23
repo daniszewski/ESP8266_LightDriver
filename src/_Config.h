b6abb0dce8e36031723f460ec3a773dc7f5142c8
Mariusz Daniszewski <daniszewski@gmail.com>
2020-08-23 17:17:33 +0200

    Proximity sensor handling (concept)

--- src/_Config.h
+++ src/_Config.h
@@ -1,6 +1,6 @@
 #include <Stream.h>
 
-String version = "0.9.3.3";
+String version = "0.9.4.0";
 
 String AP_prefix = "POWER_"; // max length 14 because the MAC address (17 chars) will be added later and max total SSID length is 31
 String scriptsPath = "/scripts/";
--- src/src.ino
+++ src/src.ino
@@ -1,1 +1,3 @@
 // this file is here only for compatibility with Arduino.cc platform
+
+void dummy_method() {}
